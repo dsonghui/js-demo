a02030962e52283a34013162733d7f490ca1540c
David <david.deng@jcinfotech.com>
2018-12-22 22:22:36 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,2 @@
-import Store from "./Store";
-import FamilyMap from "@/family";
-
 window.onload = function () {
-    let fm = new FamilyMap(document.getElementById('app'), Store);
-    fm.build();
 }
